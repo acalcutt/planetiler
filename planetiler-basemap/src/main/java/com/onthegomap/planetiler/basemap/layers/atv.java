afce804ebb32271288c2cc9fcce1cd3b5320f620
acalcutt <acalcutt@techidiots.net>
2022-02-16 01:00:07 -0500

    Update atv.java

--- planetiler-basemap/src/main/java/com/onthegomap/planetiler/basemap/layers/atv.java
+++ planetiler-basemap/src/main/java/com/onthegomap/planetiler/basemap/layers/atv.java
@@ -19,28 +19,55 @@ public class atv implements BasemapProfile.OsmAllProcessor, Layer {
 
   @Override
   public void processAllOsm(SourceFeature feature, FeatureCollector features) {
-    if (feature.canBeLine() && feature.hasTag("atv", "motorcycle")) {
-      features.line(LAYER_NAME)
-        .setBufferPixels(BUFFER_SIZE)
-        .setMinZoom(4)
-        .setAttr("class", "line")
-        .setAttr("subclass", "motorcycle")
-        .setAttr("atv", feature.getTag("atv"))
-        .setAttr("motorcycle", feature.getTag("motorcycle"));
-    } else if (feature.canBeLine() && feature.hasTag("atv")) {
-      features.line(LAYER_NAME)
-        .setBufferPixels(BUFFER_SIZE)
-        .setMinZoom(4)
-        .setAttr("class", "line")
-        .setAttr("subclass", "atv")
-        .setAttr("atv", feature.getTag("atv"));
-    } else if (feature.canBeLine() && feature.hasTag("ohv")) {
-      features.line(LAYER_NAME)
-        .setBufferPixels(BUFFER_SIZE)
-        .setMinZoom(4)
-        .setAttr("class", "line")
-        .setAttr("subclass", "ohv")
-        .setAttr("ohv", feature.getTag("ohv"));
+    if (feature.hasTag("motorcycle") || feature.hasTag("vehicle") || feature.hasTag("4wd_only") || feature.hasTag("atv") || feature.hasTag("snowmobile") || feature.hasTag("ohv") || feature.hasTag("maxwidth")) {
+      
+      
+      if (feature.canBeLine()) {
+        features.line(LAYER_NAME)
+          .setBufferPixels(BUFFER_SIZE)
+          .setMinZoom(3)
+          .setAttr("class", "line")
+          .setAttr("name", feature.getTag("name"))
+          .setAttr("motorcycle", feature.getTag("motorcycle"))
+          .setAttr("vehicle", feature.getTag("vehicle"))
+          .setAttr("4wd_only", feature.getTag("4wd_only"))
+          .setAttr("atv", feature.getTag("atv"))
+          .setAttr("snowmobile", feature.getTag("snowmobile"))
+          .setAttr("toll", feature.getTag("toll"))
+          .setAttr("ice_road", feature.getTag("ice_road"))
+          .setAttr("operator", feature.getTag("operator"))
+          .setAttr("website", feature.getTag("website"))
+          .setAttr("grade", feature.getTag("grade"))
+          .setAttr("maxwidth", feature.getTag("maxwidth"))
+          .setAttr("maxspeed", feature.getTag("maxspeed"))
+          .setAttr("ohv", feature.getTag("ohv"));
+      } else if (feature.canBePolygon()) {
+        features.polygon(LAYER_NAME)
+          .setBufferPixels(BUFFER_SIZE)
+          .setMinZoom(3)
+          .setAttr("class", "polygon")
+          .setAttr("name", feature.getTag("name"))
+          .setAttr("motorcycle", feature.getTag("motorcycle"))
+          .setAttr("vehicle", feature.getTag("vehicle"))
+          .setAttr("4wd_only", feature.getTag("4wd_only"))
+          .setAttr("atv", feature.getTag("atv"))
+          .setAttr("snowmobile", feature.getTag("snowmobile"))
+          .setAttr("ohv", feature.getTag("ohv"));
+      } else if (feature.isPoint()) {
+        features.point(LAYER_NAME)
+          .setBufferPixels(BUFFER_SIZE)
+          .setMinZoom(3)
+          .setAttr("class", "point")
+          .setAttr("name", feature.getTag("name"))
+          .setAttr("motorcycle", feature.getTag("motorcycle"))
+          .setAttr("vehicle", feature.getTag("vehicle"))
+          .setAttr("4wd_only", feature.getTag("4wd_only"))
+          .setAttr("atv", feature.getTag("atv"))
+          .setAttr("snowmobile", feature.getTag("snowmobile"))
+          .setAttr("amenity", feature.getTag("amenity"))
+          .setAttr("tourism", feature.getTag("amenity"))
+          .setAttr("ohv", feature.getTag("ohv"));
+      }
     }
   }
 
